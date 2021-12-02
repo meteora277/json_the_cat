5c8834055bddcb9298602c2e1f43cd49527030bf
meteora277 <justin.s.diaz@gmail.com>
2021-12-02 13:09:39 -0500

    fixed bug in logic where status would always be 200 even if invalid breed was passed through

--- fetchBreedDescription.js
+++ fetchBreedDescription.js
@@ -4,11 +4,14 @@ const fetchBreedDescription = function(breed, callback) {
 
   request.get(`https://api.thecatapi.com/v1/breeds/search?name=${breed}`, (error, status, body) =>{
 
-    if (error) {
+    if (JSON.parse(status.body).length === 0) {
+      error = "no cats";
       callback(error, null);
+    } else {
+      
+      let description = JSON.parse(body)[0].description;
+      callback(null, description);
     }
-    let description = JSON.parse(body)[0].description;
-    callback(null, description);
   });
 
 };
