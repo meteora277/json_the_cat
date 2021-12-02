15e1b86f10bcea142aa8bdd6ab936d07654c7f4b
meteora277 <justin.s.diaz@gmail.com>
2021-12-02 01:49:42 -0500

    created logic for get requests to the catapi server

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,26 @@
-const request = require('request');
-console.log(request);
+const args = process.argv.slice(2);
+const request = require("request");
+
+const breedFetcher = function (breed) {
+  request.get(
+    `https://api.thecatapi.com/v1/breeds/search?name=${breed}`,
+    (error, status, body) => {
+      // console.log(error)
+      // console.log(body)
+      if (error) {
+        console.log(error);
+        console.log('cat not found :(');
+      } else {
+        if (JSON.parse(status.body).length !== 0) {
+          let object = JSON.parse(body);
+          let { name, description } = object[0];
+          console.log(`${name} - ${description}`);
+        } else {
+          console.log("uwu nothing there");
+        }
+      }
+    }
+  );
+};
+
+breedFetcher(args);
